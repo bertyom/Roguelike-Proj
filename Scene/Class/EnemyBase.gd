de73d615a2c2225c9f54259b6ac80d60d2cf4e1c
bertyom <bertyom@gmail.com>
2024-10-08 22:08:45 +0400

    WIP Revamping enemy logic and AI states

--- Scene/Class/EnemyBase.gd
+++ Scene/Class/EnemyBase.gd
@@ -9,15 +9,45 @@ class_name EnemyBase
 @onready var navigation_agent: NavigationAgent2D = $NavigationAgent2D
 @onready var detection_area: Area2D = $DetectionArea
 @onready var attack_area: Area2D = $AttackArea
+@onready var hp_bar: TextureProgressBar = $EnemyHPBar
+@onready var ai_states: Node2D = $States
+
 
 #Universal humanoid stats
 @export var max_hp: int = 100
+@export var knockback_resistance: int = 2
 @export var movement_speed: float = 100.0
 @export var detection_radius: float = 200.0
 @export var attack_radius: float = 50.0
 
 #Operational variables
 var current_hp: int
+var starting_position: Vector2 
 #var current_state: State
 var target: Node2D
 var knockback_velocity: Vector2 = Vector2.ZERO
+
+func ready():
+	ai_states.parent_node = self
+	starting_position = get_global_position()
+	update_hp_bar()
+
+func take_damage(amount: int, knockback: float, attacker_position: Vector2):
+	current_hp -= amount
+	update_hp_bar()
+	if knockback > 0:
+		apply_knockback(knockback, attacker_position)
+		if knockback > knockback_resistance:
+			ai_states.change_state("Knockback")
+	if current_hp <= 0:
+		ai_states.change_state("Dead")
+		
+func apply_knockback(force: float, source_position: Vector2):
+	var knockback_direction = (global_position - source_position).normalized()
+	knockback_velocity = knockback_direction * force
+
+func update_hp_bar():
+	hp_bar.max_value = max_hp
+	hp_bar.value = current_hp
+	if current_hp == max_hp or current_hp == 0:
+		hp_bar.visible = false
--- Scene/Entity/Enemy/Components/AI/AI Container.gd
+++ Scene/Entity/Enemy/Components/AI/AI Container.gd
@@ -1,8 +1,35 @@
 extends Node
 
-@onready var parent_node = $".."
+@export var states = []
+
+@export var active_state: Node
+@export var previous_state: Node
+@onready var parent_node: CharacterBody2D
+@onready var state_timer: Timer = $"State Timer"
 
 func _ready():
 	for child in get_children():
-		if child.name != parent_node.active_state:
-			child.set_physics_process(false)
+		if child is AI_State:
+			if child.name != active_state.name:
+				child.set_physics_process(false)
+
+func setup_states():
+	for state in get_children():
+		states.append(state.name)
+
+func change_state(new_state: String):
+	if active_state:
+		active_state.exit()
+	
+	active_state = self.get_node(new_state)
+	active_state.enter()
+
+func temp_change_state(new_state: String, duration: float):
+	previous_state = active_state
+	change_state(new_state)
+	state_timer.start(duration)
+
+func _on_state_timer_timeout():
+	if previous_state:
+		change_state(previous_state.name)
+		previous_state = null
